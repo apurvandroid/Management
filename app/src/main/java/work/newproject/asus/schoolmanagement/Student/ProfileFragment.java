da9846b73c8bfd98fb0754480e777b2f045b1c48
inocentapurv <apurav12345@gmail.com>
2019-08-08 16:12:15 +0530

    update

--- app/src/main/java/work/newproject/asus/schoolmanagement/Student/ProfileFragment.java
+++ app/src/main/java/work/newproject/asus/schoolmanagement/Student/ProfileFragment.java
@@ -66,7 +66,7 @@ public class ProfileFragment extends Fragment {
         txtcontactmobilenumber=view.findViewById(R.id.txtcontactmobilenumber);
 
 
-        data="Apurv Singh";
+        data="Singh";
         studentDetails(sID);
 
 
